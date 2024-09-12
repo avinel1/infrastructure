90ccf73ed279298487318f3e707c0060957dd253
Andrei Vinel <andrei.vinel@mettle.co.uk>
2024-09-12 20:53:09 +0200

    feat: add test nginx chart

--- terraform/output.tf
+++
@@ -1,5 +0,0 @@
-resource "local_file" "kubeconfig" {
-  depends_on = [linode_lke_cluster.lke_cluster]
-  filename   = "kube-config"
-  content    = base64decode(linode_lke_cluster.lke_cluster.kubeconfig)
-}
